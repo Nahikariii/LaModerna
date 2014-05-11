e99012647172b3a0ef64702ed6faf23fca2582f4
Nahikariii <nahika77@gmail.com>
2014-05-11 11:45:56 +0100

    Dynamic code for views

--- app/src/views/highlights_view.js
+++ app/src/views/highlights_view.js
@@ -1,12 +1,33 @@
 var backbone = require('backbone');
 
+var data = [
+    {
+		'title': 'Pizza',
+		'photo' : 'holder.js/140x140',
+		'content' : 'El secreto esta en la mas. Cupcake ipsum dolor sit amet gummies. I love biscuit halvah powder chocolate bar gummies gummi bears. Gummi bears applicake croissant lemon drops jelly. Tart pastry soufflé unerdwear.com unerdwear.com jujubes',
+		'continue' : 'Leer más'
+    },
+    {
+		'title': 'Ofrecemos',
+		'photo' : 'holder.js/140x140',
+		'content' : 'Contratos personalizados, amplia gama de productos y clientes satisfechos.upcake ipsum dolor sit amet gummies. I love biscuit halvah powder chocolate bar gummies gummi bears. Gummi bears applicake croissant lemon drops jelly. Tart pastry soufflé unerdwear.com unerdwear.com jujubes',
+		'continue' : 'Informáte'
+    },
+    {
+		'title': 'Contactanos',
+		'photo' : 'holder.js/140x140',
+		'content' : 'Cupcake ipsum dolor sit amet gummies. I love biscuit halvah powder chocolate bar gummies gummi bears. Gummi bears applicake croissant lemon drops jelly. Tart pastry soufflé unerdwear.com unerdwear.com jujubes.',
+		'continue' : 'Leer más'
+    }
+]
+
 var HighlightsView = backbone.View.extend({
 	initialize: function(){
 		this.render();
 	},
 	template: require('../../views/highlights.hbs'),
 	render: function(){
-		this.$el.html(this.template());
+		this.$el.html(this.template({highlight: data}));
 	}
 });
 
--- app/src/views/slider_view.js
+++ app/src/views/slider_view.js
@@ -1,12 +1,28 @@
 var backbone = require('backbone');
 
+var data = [
+    {
+		'title': 'Productos artesanales',
+		'photo' : 'holder.js/900x200',
+		'active': true,
+    },
+    {
+		'title': 'Matería prima de primera calidd',
+		'photo' : 'holder.js/900x200',
+    },
+    {
+		'title': 'Muchas cosas',
+		'photo' : 'holder.js/900x200',
+    }
+]
+
 var SliderView = backbone.View.extend({
 	initialize: function(){
 		this.render();
 	},
 	template: require('../../views/slider.hbs'),
 	render: function(){
-		this.$el.html(this.template());
+		this.$el.html(this.template({slider: data}));
 	}
 });
 
