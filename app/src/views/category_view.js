4d5582685efe5d06c06263f71bc2bb9f647d45b4
Nahikariii <nahika77@gmail.com>
2014-05-15 19:37:50 +0100

    finish category view

--- app/src/views/category_view.js
+++ app/src/views/category_view.js
@@ -1,15 +1,17 @@
 var backbone = require('backbone');
 var HeaderView = require('./header_view');
+var ContentHeaderView = require('./content_header');
 
 var CategoryView = backbone.View.extend({
-	initialize: function(){
-		this.render();
+	initialize: function(initData){
+		this.render(initData.category);
 	},
 	template: require('../../views/category.hbs'),
-	render: function(){
-        this.$el.html(this.template());
-		var header_view = new HeaderView({el: $(".header")});
-	}
+	render: function(category){
+		products = [];
+        this.$el.html(this.template({category: category, products: products}));
+		var headerView = new HeaderView({el: $(".header")});
+		var contentHeader = new ContentHeaderView({el: $("#content-header"), headerText: category}); }
 });
 
 module.exports = CategoryView;
