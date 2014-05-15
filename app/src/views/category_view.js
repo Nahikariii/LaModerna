08a2bed50c08820630cc66a00426e8fcee20b488
Nahikariii <nahika77@gmail.com>
2014-05-15 20:17:54 +0100

    Make view dinamic and add style to template

--- app/src/views/category_view.js
+++ app/src/views/category_view.js
@@ -2,6 +2,29 @@ var backbone = require('backbone');
 var HeaderView = require('./header_view');
 var ContentHeaderView = require('./content_header');
 
+var data = [
+    {
+		'productTitle': 'Producto 1',
+	    'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
+		'photo' : 'holder.js/140x140',
+    },
+    {
+		'productTitle': 'Producto 2',
+	    'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
+		'photo' : 'holder.js/140x140',
+    },
+    {
+		'productTitle': 'Producto 3',
+	    'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
+		'photo' : 'holder.js/140x140',
+    },
+	{
+		'productTitle': 'Producto 4',
+	    'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
+		'photo' : 'holder.js/140x140',
+    }
+]
+
 var CategoryView = backbone.View.extend({
 	initialize: function(initData){
 		this.render(initData.category);
@@ -9,7 +32,7 @@ var CategoryView = backbone.View.extend({
 	template: require('../../views/category.hbs'),
 	render: function(category){
 		products = [];
-        this.$el.html(this.template({category: category, products: products}));
+        this.$el.html(this.template({category: category, products: data}));
 		var headerView = new HeaderView({el: $(".header")});
 		var contentHeader = new ContentHeaderView({el: $("#content-header"), headerText: category}); }
 });
