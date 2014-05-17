b6bdd8a6f3c10690d32fd8909b7475c9c9c7c379
Nahikariii <nahika77@gmail.com>
2014-05-17 14:06:46 +0100

    Update data structure

--- app/src/views/category_view.js
+++ app/src/views/category_view.js
@@ -2,28 +2,33 @@ var backbone = require('backbone');
 var HeaderView = require('./header_view');
 var ContentHeaderView = require('./content_header');
 
-var data = [
-    {
-		'productTitle': 'Producto 1',
-	    'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
-		'photo' : 'holder.js/140x140',
-    },
-    {
-		'productTitle': 'Producto 2',
-	    'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
-		'photo' : 'holder.js/140x140',
-    },
-    {
-		'productTitle': 'Producto 3',
-	    'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
-		'photo' : 'holder.js/140x140',
-    },
-	{
-		'productTitle': 'Producto 4',
-	    'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
-		'photo' : 'holder.js/140x140',
-    }
-]
+var data = {
+	'panaderia' : {
+		'photo': 'holder.js/1600x400',
+		'products':	[
+			{
+				'productTitle': 'Producto 1',
+				'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
+				'photo' : 'holder.js/140x140',
+			},
+			{
+				'productTitle': 'Producto 2',
+				'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
+				'photo' : 'holder.js/140x140',
+			},
+			{
+				'productTitle': 'Producto 3',
+				'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
+				'photo' : 'holder.js/140x140',
+			},
+			{
+				'productTitle': 'Producto 4',
+				'description' : 'Cupcake ipsum dolor sit amet. Jelly beans dragée gummi bears unerdwear.',
+				'photo' : 'holder.js/140x140',
+			}
+		]
+	}
+}
 
 var CategoryView = backbone.View.extend({
 	initialize: function(initData){
@@ -32,9 +37,10 @@ var CategoryView = backbone.View.extend({
 	template: require('../../views/category.hbs'),
 	render: function(category){
 		products = [];
-        this.$el.html(this.template({category: category, products: data}));
+        this.$el.html(this.template({category: category, products: data[category].products}));
 		var headerView = new HeaderView({el: $(".header")});
-		var contentHeader = new ContentHeaderView({el: $("#content-header"), headerText: category}); }
+		var contentHeader = new ContentHeaderView({el: $("#content-header"), headerText: category, photo: data[category].photo });
+	}
 });
 
 module.exports = CategoryView;
