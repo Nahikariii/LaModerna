0dddd5fbaa2b6b8d1014f2dd67047bc1fe865067
Nahikariii <nahika77@gmail.com>
2014-05-17 14:07:19 +0100

    Change init args

--- app/src/views/content_header.js
+++ app/src/views/content_header.js
@@ -2,11 +2,11 @@ var backbone = require('backbone');
 
 var ContentHeaderView = backbone.View.extend({
 	initialize: function(initData){
-		this.render(initData.headerText);
+		this.render(initData.headerText, initData.photo);
 	},
 	template: require('../../views/contentheader.hbs'),
-	render: function(headerText){
-        this.$el.html(this.template({text: headerText}));
+	render: function(headerText, photo){
+        this.$el.html(this.template({text: headerText, photo: photo}));
 	}
 });
 
