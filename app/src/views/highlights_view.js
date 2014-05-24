18384970773cc5869d92d38b2c67ba50ea42119d
Nahikariii <nahika77@gmail.com>
2014-05-24 13:43:47 +0100

    Style + Fix accents

--- app/src/views/highlights_view.js
+++ app/src/views/highlights_view.js
@@ -6,21 +6,21 @@ var data = [
 		 //"photo" : "holder.js/140x140",
 		 "photo" : "/images/pizza.jpg",
 		"content" : "El secreto esta en la mas. Cupcake ipsum dolor sit amet gummies. I love biscuit halvah powder chocolate bar gummies gummi bears. Gummi bears applicake croissant lemon drops jelly. Tart pastry soufflé unerdwear.com unerdwear.com jujube",
-		"continue" : "Leer m�s",
+		"continue" : "Leer más",
 		"href": "#/pizza"
     },
     {
 		"title": "Ofrecemos",
 		"photo" : "/images/pizza.jpg",
 		"content" : "Contratos personalizados, amplia gama de productos y clientes satisfechos.upcake ipsum dolor sit amet gummies. I love biscuit halvah powder chocolate bar gummies gummi bears. Gummi bears applicake croissant lemon drops jelly. Tart pastry soufflé unerdwear.com unerdwear.com jujube",
-		"continue" : "Informáe",
+		"continue" : "Informáte",
 		"href": "#"
     },
     {
 		"title": "Contactanos",
 		"photo" : "/images/pizza.jpg",
 		"content" : "Cupcake ipsum dolor sit amet gummies. I love biscuit halvah powder chocolate bar gummies gummi bears. Gummi bears applicake croissant lemon drops jelly. Tart pastry soufflé unerdwear.com unerdwear.com jujubes",
-		"continue" : "Leer m�s",
+		"continue" : "Leer más",
 		"href": "#"
     }
 ]
