ba0d789d3686c2086bd66b425156038163780b79
Nahikariii <nahika77@gmail.com>
2014-06-07 11:24:25 +0100

    remove debug

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -3,12 +3,10 @@ var Handlebars = require("hbsfy/runtime");
 Handlebars.registerHelper('shop-block', function(index, action, options) {
 	if (action == 'open' && ( index == 0 || index == 4))
 	{
-		console.log("ooopen");
 		return options.fn(this);
     }
 	if (action == 'close' && ( index == 3 || index == 7))
 	{
-		console.log("clooose");
 		return options.fn(this);
     }
 	return options.inverse(this);
