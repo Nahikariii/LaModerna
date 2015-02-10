d21f59c1535a31a5dda68360da585e64f0863984
Nahikariii <nahika77@gmail.com>
2015-02-10 18:49:54 +0000

    Remove placeholder text

--- app/src/views/highlights_view.js
+++ app/src/views/highlights_view.js
@@ -3,23 +3,22 @@ var backbone = require("backbone");
 var data = [
     {
 		"title": "Pizza",
-		 //"photo" : "holder.js/140x140",
 		"photo" : "/images/pizza.jpg",
-	    "content":"Wafer tart jelly muffin marzipan. Toffee chupa chups oat cake applicake oat cake jelly beans lollipop cheesecake jelly. Croissant dragée halvah. Gummies pastry chocolate cake caramels jelly dragée chocolate cake icing.",
+	    "content":"¡Nuevo! Acercate a alguna de nuestras tiendas para probar nuestra masa de pizza. Hacer una pizza casera nunca fue tan fácil, sigue nuestras instrucciones y a comer",
 		"continue" : "Leer más",
 		"href": "pizza"
     },
     {
 		"title": "Ofrecemos",
-		"photo" : "/images/pizza.jpg",
+		"photo" : "/images/pan-highlight.jpg",
 		"content" : "Como productores exclusivos de pan nos aseguramos de ofrecer una amplia gama de productos de alta calidad, desde el pan casero de toda la vida hasta panes innovadores con sabores de todo tipo.",
 		"continue" : "Informáte",
 		"href": "hosteleria"
     },
     {
 		"title": "Contactanos",
-		"photo" : "/images/pizza.jpg",
-		"content" : "Si tienes alguna duda sobre alguno de nuestros productos, te gustaría hacernos una sugerencia o simplemente quieres decirnos lo mucho que te gusta alguno de nuestros productos no dudes en ponerte en contacto con nosotros. Estaremos encantados de atenderte.",
+		"photo" : "/images/typing.jpg",
+		"content" : "Si tienes alguna duda sobre alguno de nuestros productos, te gustaría hacernos una sugerencia o simplemente quieres decirnos lo mucho que te gusta alguno de nuestros panes no dudes en ponerte en contacto con nosotros. Estaremos encantados de escucharte.",
 		"continue" : "Leer más",
 		"href": "contacto"
     }
